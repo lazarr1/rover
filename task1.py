c42803ac63e1987d84390e219188ed70069bd51c
Luke <lazarluke02@gmail.com>
2022-05-15 13:45:45 +1000

    logic

--- task1.py
+++ task1.py
@@ -14,15 +14,18 @@ import sonarTest
 def run():
     rover.init(1)
 
+    #left is index 1
+    turnCount = [0,0]
 
     while(1):
-        if(rover.getDistance() > 5):
+        if(rover.getDistance() > 10):
             rover.forward(20)
+
+
         else:
-
-
             distance = []
 
+            #distance left is index 1
 
             pca9685.setServo(5, 575)
             distance.append(rover.getDistance())
@@ -30,10 +33,15 @@ def run():
             distance.append(rover.getDistance())
             pca9685.setServo(5, 375)
 
-            if(distance[0] > distance[1] and distance[0] > 5):
+            if( (distance[0] > distance[1] and turnCount[0] < 2) or turnCount[1] == 2):
                 rover.spinRight(20)
-            else:
+                turnCount[1] =0
+                turnCount[0] += 1
+            elif( (distance[1] > distance[0] and turnCount[1] < 2) or turnCount[0] == 2  ):
                 rover.spinLeft(20)
+                turnCount[0] = 0
+                turnCount[1] += 1 
+            
 
         if(KeyboardInterrupt):
             motorTest.userInputs
